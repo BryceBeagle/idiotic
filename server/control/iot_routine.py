71203d1fdf8c6f00bd15270c1839327d67588e7e
ignormies <bryce.beagle@gmail.com>
2018-01-14 21:04:04 -0700

    IotRoutine is now callable by IotTrigger
    may change in the future

--- server/control/iot_routine.py
+++ server/control/iot_routine.py
@@ -11,17 +11,42 @@ class IotRoutine:
     Each IotRoutine has only one trigger, but multiple IotRoutines can have the same IotEvent, with
     different triggers
 
+    # TODO: Support for multiple triggers?
+
     Conditionals can be tied to individual IotEvents within the IotRoutine, or to the entire
     IotRoutine
     """
 
     def __init__(self, trigger=None, events=None, conditionals=None):
 
-        self._trigger = self.trigger(trigger)
-        self._events = self.events(events)
-        self._conditionals = self.conditionals(conditionals)
+        # Will be overridden by properties below, but defining her for PEP8 compliance
+        self._trigger = None
+        self._events = []
+        self._conditionals = {None: []}
+
+        # Use properties
+        self.trigger = trigger
+        self.events = events
+        self.conditionals = conditionals
 
         self._name = None
+
+    def __call__(self, *args, **kwargs):
+        """Trigger all events in routine"""
+
+        # Conditionals that apply to entire IotRoutine (event is []) must all pass
+        if not all(conditional() for conditional in self.conditionals if not self.conditionals[conditional]):
+            return
+
+        for conditional in self.conditionals:
+
+            # Conditional must be True
+            if not conditional():
+                return
+
+            # TODO: Use conditionals
+            for event in self.events:
+                event()
 
     @property
     def trigger(self) -> IotTrigger:
@@ -31,6 +56,8 @@ class IotRoutine:
     @trigger.setter
     def trigger(self, trigger: IotTrigger) -> None:
         """Set trigger"""
+        if not isinstance(trigger, IotTrigger):
+            raise Exception("trigger is not an instance of IotTrigger")
         self._trigger = trigger
 
     @property
@@ -39,21 +66,32 @@ class IotRoutine:
         return self._events
 
     @events.setter
-    def events(self, events: List[IotEvent]) -> None:
-        """Set events"""
-        self._events = events
+    def events(self, events: Union[IotEvent, List[IotEvent]]) -> None:
+        """Set event(s)"""
+        if isinstance(events, IotEvent):
+            self._events = [events]
+        elif isinstance(events, list):
+            if not all(isinstance(event, IotEvent) for event in events):
+                raise Exception("event is not an instance of IotEvent")
+            self._events = events
+        else:
+            raise Exception("event is not an instance of IotEvent")
 
-    def add_event(self) -> None:
+    def add_event(self, event: IotEvent) -> None:
         """Add another event"""
-        pass
+        if not isinstance(event, IotEvent):  # TODO: Support for list of events?
+            raise Exception("event is not an instance of IotEvent")
+        self._events.append(event)
 
-    def remove_event(self) -> None:
+    def remove_event(self, event: IotEvent) -> None:
         """Remove an event"""
-        pass
+        if not isinstance(event, IotEvent):  # TODO: Support for list of events?
+            raise TypeError("event is not an instance of IotEvent")
+        self._events.remove(event)
 
     @property
-    def conditionals(self) -> Dict[IotConditional, IotEvent]:
-        "Get conditionals pattern"
+    def conditionals(self) -> Dict[Union[None, IotConditional], List[IotEvent]]:
+        """Get conditionals pattern"""
         return self._conditionals
 
     @conditionals.setter
@@ -79,15 +117,15 @@ class IotRoutine:
         """
         # No conditionals
         if conditionals is None:
-            self._conditionals = {None, None}
+            self._conditionals = {None: []}
 
         # Single conditional for entire IotRoutine
         elif isinstance(conditionals, IotConditional):
-            self._conditionals = {[conditionals], None}
+            self._conditionals = {conditionals: []}
 
         # List of conditionals for entire IotRoutine
         elif isinstance(conditionals, list):
-            self._conditionals = {conditionals, None}
+            self._conditionals = dict((conditional, []) for conditional in conditionals)
 
         # Dict of conditionals and IotEvents
         elif isinstance(conditionals, dict):
@@ -100,13 +138,12 @@ class IotRoutine:
                 # List of IotEvents
                 elif isinstance(conditionals[key], list):
                     if not all(isinstance(item, IotEvent) for item in conditionals[key]):
-                        raise ValueError(f"Improper conditionals format")
+                        raise TypeError(f"Improper conditionals format")
 
                 else:
                     raise ValueError(f"Improper conditionals format")
+
                 self._conditionals = conditionals
 
         else:
-            raise ValueError(f"Improper conditionals format")
-
-        self._conditionals = conditionals
+            raise TypeError(f"Improper conditionals format")
