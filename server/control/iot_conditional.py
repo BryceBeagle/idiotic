87522bc96fefd708eb7be8ec879d410c31d4e45f
brycebeagle <bryce.beagle@gmail.com>
2018-01-25 14:43:36 -0700

    Conditionals are functions for now

--- server/control/iot_conditional.py
+++ server/control/iot_conditional.py
@@ -3,12 +3,6 @@ import warnings
 
 class IotConditional:
 
-    EQUALS = "=="
-    LT = "<"
-    LT_EQUALS = "<="
-    GT = ">"
-    GT_EQUALS = ">="
-
     def __init__(self, value1, check, value2=None):
         self.value1 = value1
         self.check = check
@@ -33,19 +27,22 @@ class IotConditional:
         # else:
         #     raise Exception("Attempted conditional evaluation without values")
 
-        if self.check is True:
-            return self.value1() is True
-        if self.check is False:
-            return self.value1() is False
+        return self.check(self)
 
-        if self.check == self.EQUALS:
-            return self.value1() == self.value2()
-        if self.check == self.LT:
-            return self.value1() < self.value2()
-        if self.check == self.LT_EQUALS:
-            return self.value1() <= self.value2()
-        if self.check == self.GT:
-            return self.value1() > self.value2()
-        if self.check == self.GT_EQUALS:
-            return self.value1() >= self.value2()
+    def equals(self):
+        return self.value1() == self.value2()
 
+    def not_equals(self):
+        return self.value1() != self.value2()
+
+    def lt(self):
+        return self.value1() < self.value2()
+
+    def lt_equals(self):
+        return self.value1() <= self.value2()
+
+    def gt(self):
+        return self.value1() > self.value2()
+
+    def gt_equals(self):
+        return self.value1() >= self.value2()
