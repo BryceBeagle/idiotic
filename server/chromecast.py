9558885fb2142ddae46f04d9fab5a283054a4e12
brycebeagle <bryce.beagle@gmail.com>
2018-04-16 16:07:57 -0700

    Removed old, unused files

--- server/chromecast.py
+++
@@ -1,9 +0,0 @@
-import os
-
-with open('state') as fi:
-    state = fi.readline()
-
-if state == 'open':
-    os.system("youtube-dl -o - https://youtu.be/oTRZOYTev4g | castnow --quiet -")
-elif state == 'closed':
-    os.system("youtube-dl -o - https://youtu.be/3znZk8BXN9Q | castnow --quiet -")
--- server/idiotic_server.wsgi
+++
@@ -1,5 +0,0 @@
-import sys
-
-sys.path.append('/home/ubuntu/idiotic-server/server')
-
-from idiotic_server import app as application
